5302fb16df5500cfc94a4ccbefc983c9437c8e82
Viktor A <99viktor.agren@gmail.com>
2024-09-18 23:08:22 +0200

    test

--- server.js
+++ server.js
@@ -1,33 +1,38 @@
 const express = require('express');
-const cors = require('cors');
 const axios = require('axios');
+const cors = require('cors');
+const path = require('path');
 
 const app = express();
+const PORT = process.env.PORT || 3001;
+
 app.use(cors());
+app.use(express.static(path.join(__dirname, 'build')));
 
-const PORT = 3001;
-
-app.get('/api/element-summary/:id', async (req, res) => {
-  try {
-    const { id } = req.params;
-    const response = await axios.get(`https://fantasy.premierleague.com/api/element-summary/${id}/`);
-    res.json(response.data);
-  } catch (error) {
-    console.error('Error fetching data:', error);
-    res.status(500).json({ error: 'An error occurred while fetching data' });
-  }
-});
+const API_BASE_URL = 'https://fantasy.premierleague.com/api';
 
 app.get('/api/bootstrap-static', async (req, res) => {
   try {
-    const response = await axios.get('https://fantasy.premierleague.com/api/bootstrap-static/');
+    const response = await axios.get(`${API_BASE_URL}/bootstrap-static/`);
     res.json(response.data);
   } catch (error) {
-    console.error('Error fetching bootstrap-static data:', error);
-    res.status(500).json({ error: 'An error occurred while fetching bootstrap-static data' });
+    res.status(500).json({ error: 'Error fetching bootstrap-static data' });
   }
 });
 
+app.get('/api/element-summary/:id', async (req, res) => {
+  try {
+    const response = await axios.get(`${API_BASE_URL}/element-summary/${req.params.id}/`);
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching element-summary data' });
+  }
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 app.listen(PORT, () => {
-  console.log(`Proxy server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
